164a286b554f127d73d7a29043aa1e149de0d016
Rob <robzlegz@gmail.com>
2023-02-20 07:43:08 +0200

    alr

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ class App:
         self.matrix: list[list[int]] = self.get_matrix(self.rows)
         self.result: list[list[int]] = self.gauss(self.matrix)
 
+        print("Rezultāts: ", self.result)
+
     def get_int(self, txt: str = "Enter int: ") -> int:
         num = None
 
@@ -21,41 +23,23 @@ class App:
         matrix = []
 
         for _ in range(rows):
-            row = []
-
-            for _ in range(rows + 1):
-                item = self.get_int()
-
-                row.append(item)
-
+            row = [self.get_int() for _ in range(rows+1)]
             matrix.append(row)
 
         return matrix
 
     def gauss(self, matrix: list[list[int]]) -> list[int]:
-        vector = [item[len(item) - 1] for item in matrix]
-        matrix = [item.pop() for item in matrix]
-
-        print(vector)
-        print(matrix)
-
         n = len(matrix)
         for i in range(n):
-            max_row = i
-            for j in range(i+1, n):
-                if abs(matrix[j][i]) > abs(matrix[max_row][i]):
-                    max_row = j
+            max_row = max(range(i, n), key=lambda j: abs(matrix[j][i]))
             matrix[i], matrix[max_row] = matrix[max_row], matrix[i]
-            vector[i], vector[max_row] = vector[max_row], vector[i]
             for j in range(i+1, n):
-                factor = matrix[j][i]/matrix[i][i]
-                for k in range(i+1, n):
-                    matrix[j][k] -= factor*matrix[i][k]
-                vector[j] -= factor*vector[i]
+                factor = matrix[j][i] / matrix[i][i]
+                matrix[j][i+1:] -= factor * matrix[i][i+1:]
+                matrix[j][i] = 0
         x = np.zeros(n)
         for i in range(n-1, -1, -1):
-            s = sum(matrix[i][j]*x[j] for j in range(i+1, n))
-            x[i] = (vector[i] - s)/matrix[i][i]
+            x[i] = (matrix[i][-1] - matrix[i][i+1:].dot(x[i+1:])) / matrix[i][i]
         return x
 
 _ = App()
