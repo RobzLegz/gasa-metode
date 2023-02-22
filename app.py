08429253b1a0850e13ce893362d8e5af6c6c96d8
RobzLegz <robzlegz@gmail.com>
2023-02-22 12:35:50 +0200

    result

--- app.py
+++ app.py
@@ -1,38 +1,48 @@
 import numpy as np
+import math
 
 class App:
     def __init__(self):
-        self.rows: int = self.get_int("Enter row count: ")
-        self.matrix: list[list[int]] = self.get_matrix(self.rows)
+        self.rows: int = self.get_num("Enter row count: ", integer=True)
+        self.matrix: list[list[float]] = self.get_matrix(self.rows)
         self.result: list[float] = self.gauss(self.matrix)
 
         print("Result: ", self.result)
 
-    def get_int(self, txt: str = "Enter int: ") -> int:
+    def get_num(self, txt: str = "Enter int: ", integer=False) -> int:
         num = None
 
         while num == None:
             try:
-                num = int(input(txt))
+                if integer == True:
+                    num = int(input(txt))
+                else:
+                    num = float(input(txt))
             except:
-                num = self.get_int("Try again: ")
+                num = self.get_num("Try again: ")
 
         return num
 
     def get_matrix(self, rows: int) -> list[list[int]]:
         return [
-            [3, -6, 3, -12],
-            [2, 3, 4, 8],
-            [4, 5, -2, 4]
+            [1.17, 0.53, -0.84, 1.15],
+            [0.64, -0.72, -0.43, 0.15],
+            [0.32, 0.43, -0.93, -0.48]
         ]
 
         matrix = []
 
         for i in range(rows):
-            row = [self.get_int(f"Enter item ({i}:{j}): ") for j in range(rows+1)]
+            row = [self.get_num(f"Enter item ({i}:{j}): ") for j in range(rows+1)]
             matrix.append(row)
 
         return matrix
+
+    def floor_num(self, x: float) -> float:
+        limiter = 10000
+        floored = math.floor(x * limiter)
+
+        return floored / limiter
 
     def gauss(self, matrix: list[list[int]]) -> list[float]:
         print("Starting matrix:")
@@ -60,15 +70,16 @@ class App:
                 print(f"{factor}R{i} + R{j}")
                 matrix[j, i:] -= factor * matrix[i, i:]
 
-        print("After elimination:")
-        print(matrix)
+                print("After elimination:")
+                print(matrix)
 
         x = np.zeros(n)
 
         for i in range(n - 1, -1, -1):
             s = sum(matrix[i, j] * x[j] for j in range(i + 1, n))
-            x[i] = (matrix[i, -1] - s) / matrix[i, i]
+            result = (matrix[i, -1] - s) / matrix[i, i]
+            x[i] = self.floor_num(result)
 
         return x
-
+    
 _ = App()
